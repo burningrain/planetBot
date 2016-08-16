c24dde0d9f4804059cb8f9ff5c26d496b91fff4b
Euler <Euler>
2016-08-16 17:26:34 +0300

    Проброс либ DOT

--- map-service/src/main/java/com/github/br/starmarines/map/converter/MapConvernter.java
+++ map-service/src/main/java/com/github/br/starmarines/map/converter/MapConvernter.java
@@ -3,13 +3,16 @@ package com.github.br.starmarines.map.converter;
 import java.io.File;
 
 import com.github.br.starmarines.gamecore.api.Galaxy;
+import org.eclipse.gef4.dot.internal.DotImport;
+import org.eclipse.gef4.graph.Graph;
 
 public class MapConvernter implements Converter<File, Galaxy> {
 
 	@Override
 	public Galaxy convert(File in) {
-		
-		
+		Graph graph = new DotImport().importDot("graph { 1--2 ; 1--3 }");
+		Graph digraph = new DotImport().importDot("digraph { 1->2 ; 1->3 }");
+		System.out.println(graph.toString());
 		return null;		
 	}
 
